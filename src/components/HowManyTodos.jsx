6258873460d8640770b1ec99f823400c27f67967
Qheuss <quentin.heusse@gmail.com>
2024-04-22 11:42:23 +0200

    todos count

---
+++ src/components/HowManyTodos.jsx
@@ -0,0 +1,12 @@
+export default function HowManyTodos() {
+  const lis = document.querySelectorAll('li');
+  let count = 0;
+  for (let i = 0; i < lis.length; i++) {
+    if (!lis[i].children.checked) {
+      count += 1;
+    }
+    console.log(!lis[i].children.checked);
+  }
+
+  return <h2>Todos ({count} todos to be done)</h2>;
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+export default function TodoList() {
+  const initialTodos = [
+    {
+      id: 1,
+      name: 'My first todo',
+      done: false,
+    },
+    {
+      id: 2,
+      name: 'My second todo',
+      done: true,
+    },
+  ];
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const handleCheck = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input
+            type="checkbox"
+            id={todo.id}
+            name={todo.name}
+            checked={todo.done}
+            onChange={() => handleCheck(todo.id)}
+          />{' '}
+          {todo.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
